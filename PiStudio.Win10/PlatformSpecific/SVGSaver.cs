5f8072beefbb1f339b353204ac3fcacc8f998da2
Jakub Lichman <Jakub Lichman@DESKTOP-T0NCII1>
2017-02-15 17:26:59 +0100

    BUGFIX: Round ends of curves added. Spaces between lines removed.

--- PiStudio.Win10/PlatformSpecific/SVGSaver.cs
+++ PiStudio.Win10/PlatformSpecific/SVGSaver.cs
@@ -14,7 +14,7 @@ namespace PiStudio.Win10
     public static class SVGSaver
     {
         /// <summary>
-        /// Writes paths into image. Stretches the paths if image, the paths were drawn on, was smaller than its original size
+        /// Writes paths into image. Stretches the paths if the image is smaller than its original size
         /// </summary>
         /// <param name="stream">Image stream</param>
         /// <param name="paths">Lines from canvas</param>
@@ -43,9 +43,15 @@ namespace PiStudio.Win10
                 foreach (var p in points)
                 {
                     if (lastPoint.HasValue)
+                    {
                         AAWidthLine((int)imgWidth, (int)imgHeight, buffer, (int)Math.Round(lastPoint.Value.X * translateX),
                             (int)Math.Round(lastPoint.Value.Y * translateY), (int)Math.Round(p.X * translateX), (int)Math.Round(p.Y * translateY),
                             (float)(pathInfo.Thickness * translateX), color);
+
+                        //By placing circles at the end of each line we create "smooth" effect of the curve
+                        FillEllipseCentered((int)imgWidth, (int)imgHeight, buffer, (int)Math.Round(p.X * translateX), (int)Math.Round(p.Y * translateY),
+                            (int)(pathInfo.Thickness * translateX / 2), (int)(pathInfo.Thickness * translateX / 2), color);
+                    }
                     lastPoint = p;
                 }
             }
@@ -668,5 +674,212 @@ namespace PiStudio.Win10
             }
         }
 
+        /// <summary>
+        /// A Fast Bresenham Type Algorithm For Drawing filled ellipses http://homepage.smc.edu/kennedy_john/belipse.pdf  
+        /// With or without alpha blending (default = false).
+        /// Uses a different parameter representation than DrawEllipse().
+        /// </summary>
+        /// <param name="bmp">The WriteableBitmap.</param>
+        /// <param name="xc">The x-coordinate of the ellipses center.</param>
+        /// <param name="yc">The y-coordinate of the ellipses center.</param>
+        /// <param name="xr">The radius of the ellipse in x-direction.</param>
+        /// <param name="yr">The radius of the ellipse in y-direction.</param>
+        /// <param name="color">The color for the line.</param>
+        /// <param name="doAlphaBlend">True if alpha blending should be performed or false if not.</param>
+        public static void FillEllipseCentered(int w, int h, int[] pixels, int xc, int yc, int xr, int yr, int color, bool doAlphaBlend = false)
+        {
+            // Avoid endless loop
+            if (xr < 1 || yr < 1)
+            {
+                return;
+            }
+
+            // Skip completly outside objects
+            if (xc - xr >= w || xc + xr < 0 || yc - yr >= h || yc + yr < 0)
+            {
+                return;
+            }
+
+            // Init vars
+            int uh, lh, uy, ly, lx, rx;
+            int x = xr;
+            int y = 0;
+            int xrSqTwo = (xr * xr) << 1;
+            int yrSqTwo = (yr * yr) << 1;
+            int xChg = yr * yr * (1 - (xr << 1));
+            int yChg = xr * xr;
+            int err = 0;
+            int xStopping = yrSqTwo * xr;
+            int yStopping = 0;
+
+            int sa = ((color >> 24) & 0xff);
+            int sr = ((color >> 16) & 0xff);
+            int sg = ((color >> 8) & 0xff);
+            int sb = ((color) & 0xff);
+
+            bool noBlending = !doAlphaBlend || sa == 255;
+
+            // Draw first set of points counter clockwise where tangent line slope > -1.
+            while (xStopping >= yStopping)
+            {
+                // Draw 4 quadrant points at once
+                // Upper half
+                uy = yc + y;
+                // Lower half
+                ly = yc - y - 1;
+
+                // Clip
+                if (uy < 0) uy = 0;
+                if (uy >= h) uy = h - 1;
+                if (ly < 0) ly = 0;
+                if (ly >= h) ly = h - 1;
+
+                // Upper half
+                uh = uy * w;
+                // Lower half
+                lh = ly * w;
+
+                rx = xc + x;
+                lx = xc - x;
+
+                // Clip
+                if (rx < 0) rx = 0;
+                if (rx >= w) rx = w - 1;
+                if (lx < 0) lx = 0;
+                if (lx >= w) lx = w - 1;
+
+                // Draw line
+                if (noBlending)
+                {
+                    for (int i = lx; i <= rx; i++)
+                    {
+                        pixels[i + uh] = color; // Quadrant II to I (Actually two octants)
+                        pixels[i + lh] = color; // Quadrant III to IV
+                    }
+                }
+                else
+                {
+                    for (int i = lx; i <= rx; i++)
+                    {
+                        // Quadrant II to I (Actually two octants)
+                        pixels[i + uh] = AlphaBlendColors(pixels[i + uh], sa, sr, sg, sb);
+
+                        // Quadrant III to IV
+                        pixels[i + lh] = AlphaBlendColors(pixels[i + lh], sa, sr, sg, sb);
+                    }
+                }
+
+
+                y++;
+                yStopping += xrSqTwo;
+                err += yChg;
+                yChg += xrSqTwo;
+                if ((xChg + (err << 1)) > 0)
+                {
+                    x--;
+                    xStopping -= yrSqTwo;
+                    err += xChg;
+                    xChg += yrSqTwo;
+                }
+            }
+
+            // ReInit vars
+            x = 0;
+            y = yr;
+
+            // Upper half
+            uy = yc + y;
+            // Lower half
+            ly = yc - y;
+
+            // Clip
+            if (uy < 0) uy = 0;
+            if (uy >= h) uy = h - 1;
+            if (ly < 0) ly = 0;
+            if (ly >= h) ly = h - 1;
+
+            // Upper half
+            uh = uy * w;
+            // Lower half
+            lh = ly * w;
+
+            xChg = yr * yr;
+            yChg = xr * xr * (1 - (yr << 1));
+            err = 0;
+            xStopping = 0;
+            yStopping = xrSqTwo * yr;
+
+            // Draw second set of points clockwise where tangent line slope < -1.
+            while (xStopping <= yStopping)
+            {
+                // Draw 4 quadrant points at once
+                rx = xc + x;
+                lx = xc - x;
+
+                // Clip
+                if (rx < 0) rx = 0;
+                if (rx >= w) rx = w - 1;
+                if (lx < 0) lx = 0;
+                if (lx >= w) lx = w - 1;
+
+                // Draw line
+                if (noBlending)
+                {
+                    for (int i = lx; i <= rx; i++)
+                    {
+                        pixels[i + uh] = color; // Quadrant II to I (Actually two octants)
+                        pixels[i + lh] = color; // Quadrant III to IV
+                    }
+                }
+                else
+                {
+                    for (int i = lx; i <= rx; i++)
+                    {
+                        // Quadrant II to I (Actually two octants)
+                        pixels[i + uh] = AlphaBlendColors(pixels[i + uh], sa, sr, sg, sb);
+
+                        // Quadrant III to IV
+                        pixels[i + lh] = AlphaBlendColors(pixels[i + lh], sa, sr, sg, sb);
+                    }
+                }
+
+                x++;
+                xStopping += yrSqTwo;
+                err += xChg;
+                xChg += yrSqTwo;
+                if ((yChg + (err << 1)) > 0)
+                {
+                    y--;
+                    uy = yc + y; // Upper half
+                    ly = yc - y; // Lower half
+                    if (uy < 0) uy = 0; // Clip
+                    if (uy >= h) uy = h - 1; // ...
+                    if (ly < 0) ly = 0;
+                    if (ly >= h) ly = h - 1;
+                    uh = uy * w; // Upper half
+                    lh = ly * w; // Lower half
+                    yStopping -= xrSqTwo;
+                    err += yChg;
+                    yChg += xrSqTwo;
+                }
+            }
+        }
+
+        private static int AlphaBlendColors(int pixel, int sa, int sr, int sg, int sb)
+        {
+            // Alpha blend
+            int destPixel = pixel;
+            int da = ((destPixel >> 24) & 0xff);
+            int dr = ((destPixel >> 16) & 0xff);
+            int dg = ((destPixel >> 8) & 0xff);
+            int db = ((destPixel) & 0xff);
+
+            destPixel = ((sa + (((da * (255 - sa)) * 0x8081) >> 23)) << 24) |
+                                     ((sr + (((dr * (255 - sa)) * 0x8081) >> 23)) << 16) |
+                                     ((sg + (((dg * (255 - sa)) * 0x8081) >> 23)) << 8) |
+                                     ((sb + (((db * (255 - sa)) * 0x8081) >> 23)));
+
+            return destPixel;
+        }
     }
 }
